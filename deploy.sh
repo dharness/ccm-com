49ad4b1286417e78bfeb1c3ce012c247c5d60760
Dylan Harness <dharness@uwo.ca>
2017-10-10 22:37:40 -0400

    updated headers

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh -t root@138.197.151.119 "cd /usr/src/app/ccm-com  && git pull origin master && pm2 restart server.js"
+ssh -t root@138.197.151.119 "cd /usr/src/app/ccm-com  && git pull origin master && npm install && pm2 restart server.js"
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ process.on('unhandledRejection', (reason, p) => {
 messaging.init(server);
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "token, Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 app.use(bodyParser.json());
